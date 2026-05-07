d5ad5528cac6fb5444cd23be8f1d2ea46a447a72
Lucas <lucas2k6@gmail.com>
2026-05-07 20:23:14 +0100

    Fix: Force IPv4 for Gmail SMTP (fix ENETUNREACH)

--- email-server/index.js
+++ email-server/index.js
@@ -85,7 +85,9 @@ async function getTransporter() {
   } else {
     console.log('[EMAIL] Using Gmail SMTP (real emails will be sent)');
     transporter = nodemailer.createTransport({
-      service: 'gmail',
+      host: 'smtp.gmail.com',
+      port: 465,
+      secure: true,
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
